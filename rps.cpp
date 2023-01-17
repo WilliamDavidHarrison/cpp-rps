7475fd42d8141c7593bb38e62d0c03d3c670b53c
William Harrison <william@williamdavidharrison.com.au>
2023-01-17 17:40:25 +0800

    Update rps.cpp

--- rps.cpp
+++ rps.cpp
@@ -4,12 +4,9 @@ using namespace std;
 
 int main() {
     char playAgain = 'y';
-
     while (playAgain == 'y') {
         int aiMove = rand() % 3;
-
         string aiMoveString;
-
         if (aiMove == 0) {
             aiMoveString = "rock";
         } else if (aiMove == 1) {
@@ -17,16 +14,11 @@ int main() {
         } else {
             aiMoveString = "scissors";
         }
-
         cout << "Rock, paper, or scissors? (r/p/s): ";
-
         char playerMove;
         cin >> playerMove;
-
         cout << "AI plays " << aiMoveString << "." << endl;
-
         string result;
-
         if (playerMove == 'r') {
             if (aiMove == 0) {
                 result = "It's a tie!";
@@ -54,11 +46,9 @@ int main() {
         } else {
             result = "Invalid move. Please enter r, p, or s.";
         }
-
         cout << result << endl;
         cout << "Do you want to play again? (y/n): ";
         cin >> playAgain;
     }
-
     return 0;
 }
